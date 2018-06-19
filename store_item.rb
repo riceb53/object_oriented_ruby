d038668500607127169866c72e759d034cd7ee0e
Heather Lee <heather.k.lee@gmail.com>
2018-06-18 21:26:32 -0400

    add two items to inventory

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+# C.1 - You are the owner of a store that sells items (choose a specific type of store item to model). Each item has various properties such as name, color, price, etc.
+
+shoe1 = {:name => "pumps", :color => "red", :price => 75, :sale => false}
+
+shoe2 = {:name => "flats", :color => "black", :price => 25, :sale => true}
+
+shoe3 = {name: "sneakers", color: "multi", price: 50, sale: false}
+
+p shoe1
+p shoe2
+p shoe3
