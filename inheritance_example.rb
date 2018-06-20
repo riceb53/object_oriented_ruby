080535467efc6748d10df56536284b26fd100f3d
Heather Lee <heather.k.lee@gmail.com>
2018-06-19 22:37:02 -0400

    update to include super method

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -21,12 +21,34 @@ end
 
 class Car < Performance
 
+  attr_reader :make, :model, :year
+  attr_writer :make, :model, :year
+
+  def initialize()
+    super
+    @make = make
+    @model = model
+    @year = year
+  end
+
+
   def honk_horn
     puts "Beeeeeeep!"
   end
+
 end
 
 class Bike < Performance
+
+attr_reader :type, :weight, :color
+attr_writer :type, :weight, :color
+
+  def initialize()
+    super
+    @type = type
+    @weight = weight
+    @color = color
+  end
 
   def ring_bell
     puts "Ring ring!"
@@ -38,3 +60,6 @@ car1 = Car.new
 car.honk_horn
 bike1 = Bike.new
 bike.ring_bell
+
+# Open the inheritance_example.rb file from the previous example.
+# Add car specific attributes to the Car class (fuel, make, model, etc.) and bicycle specific attributes to the Bicycle class (speed, type, weight, etc.). Use the super method to keep the initialize methods DRY.
