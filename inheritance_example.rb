57e4a426cbc92094f5efa095cd433397a195fc9a
Brian <riceb53@gmail.com>
2018-06-19 22:53:58 -0500

    clean up spacing, add input options hash to initialize method of performance, car and bike

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,6 +1,5 @@
 class Performance
-
-  def initialize
+  def initialize(input_options)
     @speed = 0
     @direction = 'north'
   end
@@ -16,50 +15,46 @@ class Performance
   def turn(new_direction)
     @direction = new_direction
   end
-
 end
 
 class Car < Performance
-
   attr_reader :make, :model, :year
   attr_writer :make, :model, :year
 
-  def initialize()
+  def initialize(input_options)
     super
-    @make = make
-    @model = model
-    @year = year
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @year = input_options[:year]
   end
-
 
   def honk_horn
     puts "Beeeeeeep!"
   end
-
 end
 
 class Bike < Performance
+  attr_reader :type, :weight, :color
+  attr_writer :type, :weight, :color
 
-attr_reader :type, :weight, :color
-attr_writer :type, :weight, :color
-
-  def initialize()
+  def initialize(input_options)
     super
-    @type = type
-    @weight = weight
-    @color = color
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+    @color = input_options[:color]
   end
 
   def ring_bell
     puts "Ring ring!"
   end
-
 end
 
-car1 = Car.new
-car.honk_horn
-bike1 = Bike.new
-bike.ring_bell
+car1 = Car.new(make: "Ford",model: "Focus", year: 1995)
+p car1
+car1.honk_horn
+bike1 = Bike.new(type: "mountain", weight: 81, color: "red")
+p bike1
+bike1.ring_bell
 
 # Open the inheritance_example.rb file from the previous example.
 # Add car specific attributes to the Car class (fuel, make, model, etc.) and bicycle specific attributes to the Bicycle class (speed, type, weight, etc.). Use the super method to keep the initialize methods DRY.
