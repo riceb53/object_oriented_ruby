e7c446ba3ee9b3f2c12089bded07453ace78c2b0
Heather Lee <heather.k.lee@gmail.com>
2018-06-19 21:04:41 -0400

    alter code to incorporate inheritance

---
+++ inheritance_example.rb
@@ -0,0 +1,40 @@
+class Performance
+
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+end
+
+class Car < Performance
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Performance
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+
+end
+
+car = Car.new
+car.honk_horn
+bike = Bike.new
+bike.ring_bell
